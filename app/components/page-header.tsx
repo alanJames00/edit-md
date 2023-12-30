7acee82353da5918957a4aac4cbb0165b7b0338d
alanjames00 <alanjames6282@gmail.com>
2023-12-30 21:34:38 +0530

    add page header with navbar

---
+++ app/components/page-header.tsx
@@ -0,0 +1,12 @@
+
+export default function PageHeader() {
+
+    return (
+        <div className=" mt-2 ml-2">
+            <h1 className=" text-3xl font-semibold">EDIT.md </h1>
+            <h1 className=" italic">Open, View, Edit And Do Much More With Markdown 🚀</h1>
+            
+            <hr className=" mt-4"/>
+        </div>
+    );
+}
