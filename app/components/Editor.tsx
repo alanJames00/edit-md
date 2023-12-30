92fdd61e27c45f4261e92f176ba6a08256cb4a40
alanjames00 <alanjames6282@gmail.com>
2023-12-30 21:24:15 +0530

    add codemirror editor

---
+++ app/components/Editor.tsx
@@ -0,0 +1,17 @@
+"use client"
+import CodeMirror from '@uiw/react-codemirror';
+import React from 'react';
+
+
+export default function Editor() {
+
+    return (
+        <div>
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="600px"
+        />
+        </div>
+      );
+
+}
