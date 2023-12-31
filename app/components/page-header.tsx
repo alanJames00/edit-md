0f98864871cde0764c1f480d84bcabe9b8dc6902
alanjames00 <alanjames6282@gmail.com>
2023-12-31 13:54:19 +0530

    update page header

--- app/components/page-header.tsx
+++ app/components/page-header.tsx
@@ -1,6 +1,6 @@
 import { ModeToggle } from "@/components/ThemeBtn";
 import { Button } from "@/components/ui/button"
-
+import Link from "next/link";
 
 export default function PageHeader() {
 
@@ -9,8 +9,9 @@ export default function PageHeader() {
             <h1 className=" text-3xl font-semibold">EDIT.md </h1>
             <h1 className=" italic">Open, View, Edit And Do Much More With Markdown 🚀</h1>
             
-            <div className=" flex justify-end mr-4 space-x-2">
-              <Button variant="outline">About</Button>
+            <div className=" flex justify-end mr-4 space-x-2">          
+              <Link href={'/'}><Button variant="outline">Home</Button></Link>
+              <Link href={'/about'}><Button variant="outline">About</Button></Link>
               <ModeToggle />
             </div>
 
